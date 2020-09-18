e9056e7cf9628d8e467cc11ddcecd42ecdf592c2
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 04:05:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,8 @@
-require 'pry'
-
 def my_each(array)
   counter = 0
   while counter < array.length
     yield(array[counter])
     counter += 1
   end
-  array
-end
-
-array.each do |element|
-  element + 1
+  return array
 end
